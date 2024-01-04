fc906e86ca99d9d73af3a40c6e5be8e677e56477
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2024-01-04 02:22:08 -0700

    :construction: Add excel export

--- SampleUI/src/app/app.component.ts
+++ SampleUI/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TableModule } from 'primeng/table';
+import * as XLSX from 'xlsx';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TableModule],
+  imports: [CommonModule, RouterOutlet, TableModule, 
+    ButtonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,7 +23,90 @@ export class AppComponent {
     this.products = [
       { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
       { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
+      { code: 'P1001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P1002', name: 'Product 2', category: 'Category 2', quantity: 20 },
       // ... more products
     ];
+  }
+
+  exportExcel(): void {
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.products);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Products');
+    XLSX.writeFile(wb, 'ProductsData.xlsx');
   }
 }
